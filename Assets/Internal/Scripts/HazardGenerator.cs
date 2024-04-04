ba299c35315fb5b7a69a6330d4bdf8aba1c98ce1
Nathan Roe <nateroe63@gmail.com>
2024-04-03 21:03:44 -0400

    Initial Hazard generation script

---
+++ Assets/Internal/Scripts/HazardGenerator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardGenerator : MonoBehaviour
+{
+    public Rigidbody mShip;
+    public GameObject mObject1;
+    public GameObject mObject2;
+    public GameObject mObject3;
+
+    GameObject mLastObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get ship speed and direction
+
+        // Draw a distance away from the ship
+
+        // Draw an offset from center of ship
+
+        // Draw a random object 1-3
+
+        // Instantiate new object
+
+        // Delete old and save new
+
+    }
+}
